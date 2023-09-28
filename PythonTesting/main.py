7f8e3199db06745be4865eea2f3666306bd6f6c7
fernandezmatthew <fernandezmatthew@ufl.edu>
2023-09-27 23:28:37 -0400

    9_27
    did a lot, main reflects the changes to the original python program. only a few more things to figure out and we might be able to actually get these feature implemented

--- PythonTesting/main.py
+++ PythonTesting/main.py
@@ -1,23 +1,60 @@
-from PIL import Image
-eevee = Image.open("images/eevee.png")
-# im1.save() will save the image to a new file (can change the extension here if we want
-# im1.show() will display the image using idk
+# https://www.youtube.com/watch?v=uL-wCzVMPsc&ab_channel=Keith%2CtheCoder used as reference
 
-# we can use image.blend ONLY IF we knew the size of every frame in a video and
-# converted all the thumbs to be in the bottom right corner of an image of the same size.
-# kinda cringe so i dont think so
+from PIL import Image, ImageOps
+import cv2
+import os
 
-# image.composite also requires two images of the same size, and also needs a mask. kinda interpolates
-# between the two images, at least in L mode for first argument.
+video = cv2.VideoCapture("videos/MattEmotions.mp4")
 
-# image.paste is what we're looking for!!!!!!!!!!! arg1 = image we want to paste to base, and
-# arg2 is where we want to paste that image
-thumb = Image.open("thumbs/blueThumb.png")
-# lets resize our thumb to be proportional to the background image. we want to be 1/40th of the area
-# can't use the thumbnail method because this wont allow us to make our thumbs larger if need be
-thumb.thumbnail((eevee.width // 4, eevee.height // 4))
-# let's instead paste our thumb to the bottom right of the image
-pasteCoord = (eevee.width - thumb.width, eevee.height - thumb.height)
-eevee.paste(thumb, pasteCoord, thumb.convert('RGBA'))
+try:
+    if not os.path.exists('frames'):
+        os.makedirs('frames')
+except OSError:
+    print("Error with creating directory")
 
-eevee.save("images/newEevee.png")
+length = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+currentFrame = 0
+if not os.path.exists('frames/0.png'):
+    while currentFrame < length:
+        ret, frame = video.read()
+
+        if ret:
+            name = 'frames/' + str(currentFrame) + '.png'
+            cv2.imwrite(name, frame)
+            currentFrame += 1
+
+# pre-open all the possible thumb images
+blueThumb = Image.open("thumbs/blueThumb.png")
+greenThumb = Image.open("thumbs/greenThumb.png")
+redThumb = Image.open("thumbs/redThumb.png")
+neutralThumb = Image.open("thumbs/neutralThumb.png")
+
+# inititalize our padding data
+widthPadding = None
+heightPadding = None
+padding = None
+pasteCoord = None
+for i in range(length):
+    currentFrame = Image.open("frames/" + str(i) + ".png")
+    if i == 0:
+        # resize all the thumbs this time only
+        blueThumb = ImageOps.contain(blueThumb, (currentFrame.width // 6, currentFrame.height // 6))
+        greenThumb = ImageOps.contain(greenThumb, (currentFrame.width // 6, currentFrame.height // 6))
+        redThumb = ImageOps.contain(redThumb, (currentFrame.width // 6, currentFrame.height // 6))
+        neutralThumb = ImageOps.contain(neutralThumb, (currentFrame.width // 6, currentFrame.height // 6))
+        # define our padding data this time only
+        widthPadding = currentFrame.width // 35
+        heightPadding = currentFrame.height // 35
+        padding = (widthPadding + heightPadding) // 2
+        pasteCoord = (currentFrame.width - blueThumb.width - padding, currentFrame.height - blueThumb.height - padding)
+
+    # use the info from the model or from the front end to find the current thumb for this frame
+    # to prototype, look at video and find the times to change the timestamp
+    currentThumb = blueThumb
+
+    currentFrame.paste(currentThumb, pasteCoord, currentThumb.convert('RGBA'))  # the last arg gives transparency
+    currentFrame.save("classifiedFrames/" + str(i) + ".png")
+
+# we have officially split our video into frames and classified each frame. The last step is to
+# stitch the frames back together and save as an mp4 with the original frame rate and sound
+
